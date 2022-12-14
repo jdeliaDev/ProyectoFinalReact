9cf40890d134eaf316b1be080516aef4f343b09b
jdeliaDev <jdelia@bld.com.ar>
2022-12-14 16:42:08 -0300

    actualizar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,13 @@ import './Home.css'
 
 export default function Home() {
     return (  
-        <header className = "principal-search container-fluid d-flex flex-column align-items-center justify-content-center">
+        <header className = "principal-search d-flex flex-column align-items-center justify-content-center">
             <div className= "container-header d-flex flex-column justify-content-center">
                 <h1 className = "row justify-content-center">Proyecto - Rick & Morty</h1>
                 <nav className= "row navBar align-items-center">
                     <ul className = "nav justify-content-center">
-                        <li className = "btn btn-primary">Characters</li>
-                        <li className = "btn btn-primary">Contacts</li>
+                        <li className = "btn m-2 p-3 botones">Characters</li>
+                        <li className = "btn m-2 p-3 botones">Contacts</li>
                     </ul>
                 </nav> 
             </div>
